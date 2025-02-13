85ac3807b491fe5000a1061452cc144ab43c5918
Fernando Corte Vargas <fernando.cortevargas@ticketswap.com>
2025-02-13 13:11:20 -0500

    🐛 Fix bugs

--- section-04-research-and-development/preprocessors.py
+++ section-04-research-and-development/preprocessors.py
@@ -53,3 +53,25 @@ class Mapper(BaseEstimator, TransformerMixin):
             X[feature] = X[feature].map(self.mappings)
 
         return X
+    
+
+# categorical object caster
+class CategoricalCaster(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables):
+        
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        
+        self.variables = variables
+
+    def fit(self, X, y=None):
+        # we need the fit statement to accomodate the sklearn pipeline
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].astype('O')
+
+        return X
